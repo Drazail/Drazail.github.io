247daa6a2105af70432d756bca5939a99d4fe6e6
Reza.Y.Panah <reza.y.3@gmail.com>
2021-06-04 19:12:20 +0300

    {HidenGame}

---
+++ fts/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1622821785,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/sprite-sheet3.png",
+		"images/sprite-sheet4.png",
+		"images/sprite-sheet5.png",
+		"images/sprite-sheet6.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite3-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
